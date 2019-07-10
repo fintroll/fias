ebd5f8a5966a33791a4c9c313f5f1ca3794db9ee
Fintroll <fitnroll66692@gmail.com>
2019-07-10 15:06:11 +0400

    FIAS-11 autocomplete api postalcode

--- rest/searches/SearchAddress.php
+++ rest/searches/SearchAddress.php
@@ -152,7 +152,7 @@ class SearchAddress extends Model
             $query->andWhere('0=1');
             return $dataProvider;
         }
-        $query->andWhere(['LIKE', 'POSTALCODE', $this->term]);
+        $query->andWhere(['POSTALCODE' => $this->term]);
         $query->andWhere(['>=', 'ENDDATE', date('Y-m-d')]);
         $query->orderBy(['HOUSENUM' => SORT_ASC, 'BUILDNUM' => SORT_ASC, 'STRUCNUM' => SORT_ASC]);
         $query->limit(20);
